3706e6ee7bafeb0fd7cd484698e095eb28171bf7
Alex Studnička <astudnicka@gmail.com>
2016-06-07 11:12:18 +0200

    Add Venice dependency

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "EngineioServer",
     dependencies: [
 		.Package(url: "https://github.com/Zewo/WebSocketServer.git", majorVersion: 0, minor: 1),
+		.Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 7),
 		.Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 7),
 		.Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
     ]
