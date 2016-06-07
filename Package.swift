1bbdd78e4e42f4728854c05b26286eba01340ad3
Alex Studnička <astudnicka@gmail.com>
2016-06-07 11:14:20 +0200

    Add CLibvenice dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
 		.Package(url: "https://github.com/Zewo/WebSocketServer.git", majorVersion: 0, minor: 1),
 		.Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 7),
+		.Package(url: "https://github.com/VeniceX/CLibvenice.git", majorVersion: 0, minor: 5),
 		.Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 7),
 		.Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
     ]
