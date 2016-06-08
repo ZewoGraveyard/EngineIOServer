180b98a01c03a95e86afe302da240b2bdbb2b282
Alex Studnička <astudnicka@gmail.com>
2016-06-08 14:43:44 +0200

    Update WebSocket

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "EngineioServer",
     dependencies: [
-		.Package(url: "https://github.com/Zewo/WebSocketServer.git", majorVersion: 0, minor: 1),
+		.Package(url: "https://github.com/Zewo/WebSocketServer.git", majorVersion: 0, minor: 2),
 		.Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 7),
 		.Package(url: "https://github.com/VeniceX/CLibvenice.git", majorVersion: 0, minor: 5),
 		.Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 7),
