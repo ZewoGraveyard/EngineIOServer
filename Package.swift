f2da4c18001e3ee308c84ceaf4e7aabd2fcf4021
Alex Studnička <AStudnicka@gmail.com>
2016-06-12 11:29:17 +0200

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "EngineIOServer",
     dependencies: [
-		.Package(url: "https://github.com/Zewo/WebSocketServer.git", majorVersion: 0, minor: 2),
+		.Package(url: "https://github.com/Zewo/WebSocketServer.git", majorVersion: 0, minor: 3),
 		.Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 7),
 		.Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 8),
 		.Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
